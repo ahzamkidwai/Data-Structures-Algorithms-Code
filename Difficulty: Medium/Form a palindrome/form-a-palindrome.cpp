4cd4999cb50987bda9ddf7033c9eff27dab860ea
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2024-08-03 00:04:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.cpp
@@ -1,18 +1,17 @@
 //{ Driver Code Starts
 //Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
 
-class Solution{   
-public:
+class Solution{
+  public:
     int lcs(string s, string t) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0)); // DP table initialized with 0
-    
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0)); 
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (s[i - 1] == t[j - 1]) {
@@ -24,8 +23,7 @@ public:
         }
         return dp[n][n];
     }
-    
-    int findMinInsertions(string s) {
+    int countMin(string s){
         int n = s.size();
         string t = s;
         reverse(t.begin(), t.end());
@@ -35,19 +33,17 @@ public:
 };
 
 //{ Driver Code Starts.
-
-
-
 int main(){
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
-        string S;
-        cin>>S;
+        string str;
+        cin >> str;
         Solution ob;
-        cout<<ob.findMinInsertions(S)<<endl;
+        cout << ob.countMin(str) << endl;
     }
-    return 0;
+return 0;
 }
+
 
 // } Driver Code Ends
