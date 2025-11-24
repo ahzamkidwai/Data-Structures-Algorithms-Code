caf04451d2928aeb05858e0adab0122ec715522b
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2025-11-25 00:53:31 +0530

    Time: 0 ms (100%), Space: 8.7 MB (42.94%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     int titleToNumber(string s) {
         reverse(s.begin(), s.end());
-        unordered_map<char, int> m;
-        for(int i=1; i<=26; i++){
-            m[char(i + 64)] = i;
-        }
         long long int N = 26;
         int ans = 0;
         for(int i = 0; i < s.size(); i++){
-            char ch = s[i];
-            int num = m[ch];
+            int num = int(s[i]) - 64;
             if(i == 0) ans = ans + num;
             else if(i == 1) ans = ans + (num * N);
             else {
