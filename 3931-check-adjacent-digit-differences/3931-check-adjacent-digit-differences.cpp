ccb087b842864910761cac9073c9985be6163be6
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2026-05-17 15:42:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (48.35%) - LeetHub

--- 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
-    int convertCharToNum(char ch) {
-        return (int(ch) - 48);
-    }
     bool isAdjacentDiffAtMostTwo(string s) {
-        bool f = true;
         int n = s.size();
-        for (int i=0; i<n-1; i++) {
-            int num1 = convertCharToNum(s[i]);
-            int num2 = convertCharToNum(s[i+1]);
-            int diff = abs(num1 - num2);
-            cout << "Diff : " << diff << endl;
-            if (diff > 2) return false;
+        for (int i=0; i < n-1; i++) {
+            if (abs((s[i]-'48')-(s[i+1]-'48')) > 2) return false;
         }
         return true;
     }
