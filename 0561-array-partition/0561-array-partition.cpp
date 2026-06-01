52ac853b943cb9da98d7d9fba763063d03fa6a52
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2026-06-01 13:25:10 +0530

    Time: 1 ms (93.06%), Space: 31.9 MB (85.23%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long int sum = 0;
+        for (int i=0; i<n; i=i+2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+};
